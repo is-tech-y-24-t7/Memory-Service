ecb8fcbca2f8154fd0ebe1d0dd4042737454f161
ShafikovAlexander <shafikov.alexander@gmail.com>
2022-06-08 20:34:29 +0300

    Добавьте файлы проекта.

---
+++ MemoryService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemoryService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
