cc586cc1316717359ca4e2dfdeb069f08fbbc05a
ShafikovAlexander <shafikov.alexander@gmail.com>
2022-06-27 02:17:20 +0300

    add Emulator

---
+++ MemoryService/Emulator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MemoryService
+{
+    public class Emulator
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new Ui());
+        }
+    }
+}
